d33c4b073d0474debca97fb0b56ee7933f2f54c7
Dipendra Bahadur Chand <chand.dipendra19@gmail.com>
2020-10-07 15:42:14 +0545

    mongodb database connected

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+// Database Connection
+mongoose.connect(
+    'mongodb+srv://dip3790:dip3790@cluster0.lhzre.mongodb.net/node_auth?retryWrites=true&w=majority',
+    { useNewUrlParser: true },
+    () => console.log('Database Connection Successful!'));
 
 //Import Routes
 const authRoute = require('./routes/auth');
