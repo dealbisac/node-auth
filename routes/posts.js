b457db2a44d24e36f221a3b64c0cc8b150b8fd17
Dipendra Bahadur Chand <chand.dipendra19@gmail.com>
2020-10-07 23:08:51 +0545

    final touch

--- routes/posts.js
+++ routes/posts.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const User = require('../model/User');
 const verify = require('./verifyToken');
 
 router.get('/', verify, (req, res) => {
@@ -8,6 +9,8 @@ router.get('/', verify, (req, res) => {
             description: " Welcome to the very first post of the MERN authetication, This is the backend of the system."
         }
     });
+    // res.send(req.user);
+    // User.findbyOne({ _id: req.user });
 });
 
 module.exports = router;
