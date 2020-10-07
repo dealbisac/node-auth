aa5cd2a5cc7fc5baf8b6c23f6f9008bc6ce6ecca
Dipendra Bahadur Chand <chand.dipendra19@gmail.com>
2020-10-07 17:33:16 +0545

    unique email registration

--- routes/auth.js
+++ routes/auth.js
@@ -6,8 +6,11 @@ const { registerValidation } = require('../validation');
 router.post('/register', async (req, res) => {
     // Validation of the user before making user
     const { error } = registerValidation(req.body);
-    if (error)
-        return res.status(400).send(error.details[0].message);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    //Checking if the user is already in the database
+    const emailExist = await User.findOne({ email: req.body.email });
+    if (emailExist) return res.status(400).send('Email already exists');
 
     const user = new User({
         name: req.body.name,
