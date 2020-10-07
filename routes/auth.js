224c77ffa69ab302cab21a4689ed53de97dde8fb
Dipendra Bahadur Chand <chand.dipendra19@gmail.com>
2020-10-07 17:27:54 +0545

    validation logic separated

--- routes/auth.js
+++ routes/auth.js
@@ -1,21 +1,11 @@
 const router = require('express').Router();
 const User = require('../model/User');
+const { registerValidation } = require('../validation');
 
-//Data Validation
-const Joi = require('@hapi/joi');
-
-const schema = {
-    name: Joi.string().min(6).required(),
-    email: Joi.string().min(6).required().email(),
-    password: Joi.string().min(6).required()
-};
 
 router.post('/register', async (req, res) => {
     // Validation of the user before making user
-    // const validation = schema.validate(req.body);
-    // res.send(validation);
-
-    const { error } = Joi.validate(req.body, schema);
+    const { error } = registerValidation(req.body);
     if (error)
         return res.status(400).send(error.details[0].message);
 
@@ -27,7 +17,7 @@ router.post('/register', async (req, res) => {
     try {
         const savedUser = await user.save();
         res.send(savedUser);
-
+        // res.status(201).send('User Registered Succesfully');
     } catch (err) {
         res.status(400).send(err);
     }
